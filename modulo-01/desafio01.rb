129f42bace4568bd88b1904664bb4c84aebf6f67
joelsonlins <joelsonlins@hotmail.com>
2024-09-05 20:13:37 -0300

    Criando o seu Primeiro Projeto em Ruby

---
+++ modulo-01/desafio01.rb
@@ -0,0 +1,7 @@
+puts "Digite seu nome: "
+nome = gets.chomp
+puts "Digite seu sobrenome: "
+sobrenome = gets.chomp
+puts "Digite sua idade: "
+idade = gets.chomp
+puts "Seja bem vindo(a) #{nome} #{sobrenome}, sua idade de #{idade} anos permite sua entrada!"
